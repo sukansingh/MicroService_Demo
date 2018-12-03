b002fa258bb7988524fdc31bbe33f472631af2db
sukansingh <sukansingh@gmail.com>
2018-12-03 23:58:36 +0530

    Update MicroServiceDemoApplication.java
    made changes to deploy on tomact

--- MicroService_Demo/src/main/java/com/micro/demo/MicroService_Demo/MicroServiceDemoApplication.java
+++ MicroService_Demo/src/main/java/com/micro/demo/MicroService_Demo/MicroServiceDemoApplication.java
@@ -2,9 +2,10 @@ package com.micro.demo.MicroService_Demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MicroServiceDemoApplication {
+public class MicroServiceDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServiceDemoApplication.class, args);
